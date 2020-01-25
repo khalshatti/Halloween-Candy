cbb9b41a787a164fa1f9cea4ecd3cfe6c7f55515
khalshatti <46720593+khalshatti@users.noreply.github.com>
2020-01-25 16:06:24 -0500

    Add files via upload

--- Halloween.java
+++ Halloween.java
@@ -1,38 +1,60 @@
-//***************************************************************************************//
-//    Halloween.java         Author: Khaled Alshatti                                    //
-//                                                                                       // 
-//    A program that shows what its like to hand out candy on halloween.                 //          
-//***************************************************************************************//
+//******************************************************************************************************************
+// Halloween.java    Author: Khaled Alshatti
+//
+// A program that shows what its like to hand out candy on halloween.
+//******************************************************************************************************************
+
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Halloween {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		ArrayList<TreatHouse> houses = new ArrayList<TreatHouse>();
 
-		System.out.println("Which candy should we give out first? Candy in pot 1 or candy in pot 2?");
-		int candyPot = scan.nextInt();
+		Random gen = new Random(System.currentTimeMillis());
+		int numHouse = gen.nextInt(10) + 1;
+		
+		// create houses
+		for (int i = 0; i < numHouse; i++) {
+			System.out.println("House " + i );
+			System.out.println("Which candy should we give out first? Candy in pot 1 or candy in pot 2?");
+			int candyPot = scan.nextInt();
 
-		System.out.println("How much candy did we buy?");
-		int totalCandy = scan.nextInt();
+			System.out.println("How much candy did we buy?");
+			int totalCandy = scan.nextInt();
 
-		TreatHouse ourHouse = new TreatHouse(candyPot, totalCandy);
-
-		while (ourHouse.getCandyCount() > 0) {
-			ourHouse.getCandyStatus(); //tells how much candy is left & other stuff
-
-			System.out.println("How much candy per treater should we give out?");
-			int treatsPerTreater = scan.nextInt();
-			ourHouse.setTreatsPerTreater(treatsPerTreater);
-						
-			System.out.println("Knock, knock...." + "Trick or treat!");
-			ourHouse.knockKnock();
-			ourHouse.passOutCandy();
+			houses.add( new TreatHouse(candyPot, totalCandy));
 		}
-
-		System.out.println("Time to turn off the lights and go to bed!");
-		System.out.println("The last candy came from pot number"+ ourHouse.getLastPot());
-		System.out.println("Happy Halloween!");
+		
+		boolean allDone = false;
+		while(!allDone){
+			//Each house will pass out candy for a bit.
+			for (TreatHouse house : houses) {
+				house.passTime();				
+			}
+			
+			//Check to see if all the house are done. If so set sentinel flag to true and exit while loop
+			allDone = false;
+			for(TreatHouse house: houses){
+				//If a single house is not done then all are not done
+				if(!house.isDone()){
+					allDone = false;
+					break;
+				}
+				else
+					allDone = true;
+			}
+			
+			//Call static method to display some statistics about the holiday so far
+			//calling the method to display static variable data. 
+         //Since this is a static method, it can be called directly.
+         TreatHouse.getHalloweenStatus();
+			
+		}
+		System.out.println("All houses are done");
 		scan.close();
 	}
 }
--- TreatHouse.java
+++ TreatHouse.java
@@ -1,124 +1,188 @@
-//***************************************************************************************//
-//    TreatHouse.java         Author: Khaled Alshatti                                    //
-//                                                                                       // 
-//    A program that shows what its like to hand out candy on halloween.                 //          
-//***************************************************************************************//
+//******************************************************************************************************************
+// TreatHouse.java      Author:Khaled Alshatti
+//
+// A program that shows what its like to hand out candy on halloween.
+//******************************************************************************************************************
 
 import java.util.Random;
+import java.util.Scanner;
 
-public class TreatHouse 
-{
-   int candyPot1; //amount of candy in pot 1
-	int candyPot2; //amount of candy in pot 2
-	int currentPot; //1 or 2
-	int totalCandy;
-	int currentTreaters; 
-	int treatsPerTreater;
-   int leftOverCandy;
+public class TreatHouse {
+	private int candyPot1; // amount of candy in pot 1 for a single TreatHouse
+	private int candyPot2; // amount of candy in pot 2 for a single TreatHouse
+	private int currentPot; // (1 or 2) current Pot for a single TreatHouse
+	private int totalCandy; // total candy for a single TreatHouse
+	private int currentTreaters; // current Treaters for a single TreatHouse
+	private boolean isDone = false; // is this house done for the night?
+	private int treatsPerTreater; //How much candy to give out to each treater at a single TreatHouse
+	
+   //Unique House ID
+   private int houseID;			   //This is the number for the house which is unique.
+   
+	//Declaring Static Variables
+   //Static variables
+	private static int participatingHouses; //This contains the total number of houses participating in this year's holiday
+	private static int totalCandyPassedOut; //This contains the total number of candy passed out for all the houses.
+	private static int totalTricksGiven;    //This contains the total number of tricks given out. Remember that each kid gets tricked.
+	
+   
+	//Showing Static Variable
+	//Method to display the statistic variables
+	public static void getHalloweenStatus(){
+		System.out.println("Total Number of Participating Houses: " + participatingHouses);
+      System.out.println("Total Candy Passed Out: " + totalCandyPassedOut);
+      System.out.println("Total Tricks Given: " + totalTricksGiven);      
+	}
+	
+	
 
-	public TreatHouse(int candyPot, int totalCandy) 
-   {
-		//Add code here, be sure to split the candy between the pots.
+	public TreatHouse(int candyPot, int totalCandy) {
+		// Error Check and split candy
+		if (totalCandy > 0) {
+			candyPot1 = totalCandy / 2;
+			candyPot2 = totalCandy / 2 + totalCandy % 2;
+		} else {
+			System.out.println("We can't give out candy if we don't have any."
+					+ " I think we have some from last year. Yep, we have 100 " + "" + "pieces of candy to give out.");
+			totalCandy = 100;
+			candyPot1 = totalCandy / 2;
+			candyPot2 = totalCandy / 2 + totalCandy % 2;
+		}
+
+		// currentPot Check
+		if (candyPot == 1) {
+			currentPot = 1;
+		} else if (candyPot == 2) {
+			currentPot = 2;
+		} else {
+			System.out.println("Invalid choice for pot. Only 1 or 2. Using pot 1");
+			currentPot = 1;
+		}
 		
-      currentPot = 1;
-      
-      if(candyPot ==2)
-      {
-         currentPot =2;
-      }
-      
-      if(totalCandy>0)
-      {
-         candyPot1 = (int)Math.floor(totalCandy/2);
-         
-         candyPot2 = totalCandy - candyPot1;
-      }
-      
-      else
-      {
-         System.out.println("We can't give out candy if we dont have any. I think we have " 
-                           + "some from last year. Yep, we have 100 pieces of candy  to give out.");
-         candyPot1 = 50;
-         candyPot2 = 50;
-      }
+		//Adjust Static variables as needed here:
+      //initializing static variables
+      participatingHouses++;
+      houseID = participatingHouses;
 	}
 
-	public int getCandyCount() 
-   {
+	public void passTime() {
+		Scanner scan = new Scanner(System.in);
+
+		//before
+		this.getCandyStatus();
+		
+		System.out.println("How much candy per treater should we give out?");
+		this.setTreatsPerTreater(scan.nextInt());
+
+		System.out.println("Knock, knock...." + "Trick or treat!");
+		this.knockKnock();
+		this.passOutCandy();
+		
+		//After
+		this.getCandyStatus();
+
+		//Determines if the house if done for the night.
+		if(totalCandy == 0){
+			System.out.println("Time to turn off the lights and go to bed!");
+			System.out.println("The last candy came from pot number" + this.getLastPot());
+			System.out.println("Happy Halloween!");
+			isDone = true;
+		}
+	}
+
+	private void printCurrentPot() {
+		System.out.println("The current Pot is: " + currentPot);		
+	}
+
+
+
+	public int getCandyCount() {
 		return candyPot1 + candyPot2;
 	}
 
-	public void passOutCandy() 
-   {
-		//If there are enough treats per treater for the given amount per treater, pass out candy
-		//from the current pot and switch to the other one.
-		//Else display a message that the treaters have been tricked... (no candy for them.)
-		// but do not change the current pot
-	   leftOverCandy = (currentTreaters * treatsPerTreater);
-      
-      if(currentPot == 1)
-      {
-         if(candyPot1 >= leftOverCandy)
-         {
-            candyPot1 = candyPot1 - leftOverCandy;
-            
-            if(candyPot2 > 0)
-            {
-               currentPot = 2;
-            }  
-         }
-         else
-         {
-            System.out.println("The treaters have been tricked... (no candy for them.)");
-         }
-      }
-      else 
-      {
-         if(candyPot2 >= leftOverCandy)
-         {
-            candyPot2 = candyPot2 - leftOverCandy;
-            
-            if(candyPot1 > 0)
-            {
-               currentPot = 1;
-            }
-         }
-         else
-         {
-            System.out.println("The treaters have been tricked... (no candy for them.)");
-         }
-      }	
+	public void passOutCandy() {
+		// If there are enough treats per treater for the given amount per
+		// treater, pass out candy from the current pot and switch to the other one.
+		// Else display a message that the treaters have been tricked... (no
+		// candy for them.) but do not change the current pot
+
+		if ((currentPot == 1 && candyPot1 == 0) || (currentPot == 2 && candyPot2 == 0)) {
+			// Switch pots and print message
+			System.out.println("There is no candy in the currentPot Switching pots.");
+			switchPots();
+		}
+
+		{// There is some candy to give out.
+			int currentPotAmmount = getCurrentPotAmount();
+			if (treatsPerTreater * currentTreaters <= currentPotAmmount) {
+				//Passing out Candy
+				if (currentPot == 1) {
+					candyPot1 -= treatsPerTreater * currentTreaters;
+					totalCandy -= treatsPerTreater * currentTreaters;
+				} else {
+					candyPot2 -= treatsPerTreater * currentTreaters;
+					totalCandy -= treatsPerTreater * currentTreaters;
+				}
+            //Calculting total candy passed out
+            totalCandyPassedOut += (treatsPerTreater * currentTreaters);
+				switchPots();
+			} else {
+				// trick
+            //increasing tricks counter
+            totalTricksGiven++;
+				System.out.println("You have been tricked!");
+			}
+
+		}
+
 	}
 
-	
-	//Sets the number of trick or treaters.
-	public void knockKnock() 
-   {
-	   Random gen = new Random(System.currentTimeMillis());
-		this.currentTreaters = gen.nextInt(13) + 1; //1 to 13 treaters.
-      System.out.println("Amount of Trick or Treaters: " + currentTreaters);
-	}
-	
-	//Displays how much candy in each pot, total candy left
-
-	public void getCandyStatus() 
-   {
-		//add in code here
-		System.out.println("Candy in pot 1: " + candyPot1);
-      System.out.println("Candy in pot 2: " + candyPot2);
-      System.out.println("Toatl amount of candy: " + (candyPot1 + candyPot2));
+	private int getCurrentPotAmount() {
+		if (currentPot == 1)
+			return candyPot1;
+		else
+			return candyPot2;
 	}
 
-	//returns the pot number for which candy was last given.
-	public int getLastPot() 
-   {
-		//add code here
-      return currentPot;
+	private void switchPots() {
+		if (currentPot == 1) {
+			currentPot = 2;
+		} else
+			currentPot = 1;
+
 	}
 
-	public void setTreatsPerTreater(int treatsPerTreater) 
-   {
-		//add code here
-      this.treatsPerTreater = treatsPerTreater;
+	// Sets the number of trick4 or treaters.
+	public void knockKnock() {
+		Random gen = new Random(System.currentTimeMillis());
+		this.currentTreaters = gen.nextInt(13) + 1; // 1 to 13 treaters.
+		System.out.println("There are " + currentTreaters + " treaters.");
+	}
+
+	// Displays how much candy in each pot, total candy left
+
+	public void getCandyStatus() {
+		this.printCurrentPot();
+		System.out.println("Candy in Pot 1: " + candyPot1);
+		System.out.println("Candy in Pot 2: " + candyPot2);
+
+	}
+
+	// returns the pot number for which candy was last given.
+	public int getLastPot() {
+		if (currentPot == 1)
+			return 2;
+		else {
+			return 1;
+		}
+	}
+
+	public void setTreatsPerTreater(int treatsPerTreater) {
+		this.treatsPerTreater = treatsPerTreater;
+	}
+
+	public boolean isDone() {
+				
+		return isDone;
 	}
 }
